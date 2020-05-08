1686f4590b306e1117153344447fcadb64edec0b
cuilu <cuilu@huobi.com>
2020-05-08 10:28:44 +0800

    hello world

--- README.md
+++
@@ -1,2 +0,0 @@
-# Java-Treasure
-JAVA字典
---
+++ java-foundation/src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    //创建主函数快捷键psvm
+    public static void main(String[] args) {
+        //输出快捷键sou
+        System.out.println("hello world");
+    }
+}
